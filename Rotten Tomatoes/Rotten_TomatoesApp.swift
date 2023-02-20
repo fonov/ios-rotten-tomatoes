b6da3a4b186101665d21cdca2f9610b09cbbae99
Sergei Fonov <sergeufonov@gmail.com>
2023-02-20 11:26:05 +0400

    Initial Commit

---
+++ Rotten Tomatoes/Rotten_TomatoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rotten_TomatoesApp.swift
+//  Rotten Tomatoes
+//
+//  Created by Sergei Fonov on 20.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Rotten_TomatoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
