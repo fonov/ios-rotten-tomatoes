33696ab1962032a070b42139cd3813c1b49376f4
Sergei Fonov <sergeufonov@gmail.com>
2023-03-18 16:23:16 +0400

    add scripts

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Rotten Tomatoes
+#
+#  Created by Sergei Fonov on 18.03.23.
+#  
+
+echo "Post clone script run"
---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  Rotten Tomatoes
+#
+#  Created by Sergei Fonov on 18.03.23.
+#  
+
+echo "Post xcode build script run"
